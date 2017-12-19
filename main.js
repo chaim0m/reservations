3fc7ea5b3a75de490d6ebd1ccac5a9556b9c05af
Chaim Finson <chaim0m@hotmail.com>
2017-12-19 08:19:35 +0200

    the best program ever

--- main.js
+++ main.js
@@ -3,10 +3,22 @@ var reservations = {
   'Ted': { claimed: true }
 }
 
-var name = prompt('Please enter the name for your reservation');
 
+var name = prompt('Please enter the name for your reservation');
+name = name[0].toUpperCase() + name.slice(1).toLowerCase();
 var claimReservation = function () {
-  // write your code here!
+  if (reservations[name]){
+    if (reservations[name].claimed){
+      alert('Welcome to the best restaurant ever!');
+    }
+    else{
+      alert('Your reservations was already used, Ha!');
+    }
+  }
+  else{
+    alert('You have a new reservation');
+    reservations.name = {claimed: true};
+}
 }
 
 claimReservation();
